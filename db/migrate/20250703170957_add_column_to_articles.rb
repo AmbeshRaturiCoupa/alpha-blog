e7d7ab2d9e3a18bca59e4cb16c12c8f650daca4f
AMBESH RATURI <ambesh.raturi@coupa.com>
2025-07-03 22:42:53 +0530

    added table articles

---
+++ db/migrate/20250703170957_add_column_to_articles.rb
@@ -0,0 +1,8 @@
+class AddColumnToArticles < ActiveRecord::Migration[5.2]
+  def change
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
