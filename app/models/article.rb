49c3ef52cc5296135a0da289002f04b351a1b6b0
AMBESH RATURI <ambesh.raturi@coupa.com>
2025-07-04 23:04:40 +0530

    added show page

---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ApplicationRecord
+    #gives us getters and setters
+    validates :title, presence: true, length: {minimum: 6, maximum: 100}
+    validates :description, presence: true,length: {minimum:10, maximum:300}
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_07_03_170957) do
+ActiveRecord::Schema.define(version: 2025_07_03_173232) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text "description"
   end
 
 end
