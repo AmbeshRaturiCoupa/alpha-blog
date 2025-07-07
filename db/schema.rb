8f4d3cafdeff7f84f283fbc0dbb3ac982ab00680
AMBESH RATURI <ambesh.raturi@coupa.com>
2025-07-07 22:57:38 +0530

    added users table and user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_07_03_173232) do
+ActiveRecord::Schema.define(version: 2025_07_07_172124) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
