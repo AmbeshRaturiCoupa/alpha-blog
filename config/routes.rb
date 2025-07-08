e37badd4620677e221b14581dec1e0787491922f
AMBESH RATURI <ambesh.raturi@coupa.com>
2025-07-08 23:27:27 +0530

    added sign up flow

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   root 'pages#home'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   resources :articles
 
 end
