15e22db7fe398c477d85ad78ca685f98f6da685e
AMBESH RATURI <ambesh.raturi@coupa.com>
2025-07-13 02:55:53 +0530

    added edit category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,7 +7,7 @@ class CategoriesController < ApplicationController
 
     def show
         @category=Category.find(params[:id])
-        
+        @articles=@category.articles.paginate(page: params[:page], per_page: 5)
     end
 
     def index
@@ -24,6 +24,22 @@ class CategoriesController < ApplicationController
         end
 
 
+    end
+
+    def edit
+        @category=Category.find(
+        params[:id]
+        )
+    end
+
+    def update
+        @category=Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:notice]="Category was successfully updated"
+            redirect_to @category
+        else
+            render 'edit'
+        end
     end
 
     private
