17fe6092c1c94596699a8f4a6683c23d2a7f92d2
AMBESH RATURI <ambesh.raturi@coupa.com>
2025-07-13 01:26:49 +0530

    Added Articles and Categories many to many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_07_10_192214) do
+ActiveRecord::Schema.define(version: 2025_07_12_193528) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
